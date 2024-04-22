db6373e8812fecebe1f641adf77edd340bdd2ed4
shannnl <shannnlang@qq.com>
2024-04-22 18:54:29 +0800

    post_list_layout

--- lib/post/index/components/post_list.dart
+++ lib/post/index/components/post_list.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:xb2_flutter/post/index/components/post_list_item.dart';
 import 'package:xb2_flutter/post/index/post_index_model.dart';
 
 class PostList extends StatefulWidget {
+  final String? sort;
+
+  PostList({
+    this.sort,
+  });
+
   @override
   _PostListState createState() => _PostListState();
 }
 
 class _PostListState extends State<PostList> {
+  restoreLayout() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final data = prefs.getString('postListLayout');
+
+    if (data != null) {
+      PostListLayout layout = PostListLayout.values.firstWhere((item) {
+        return item.toString() == data;
+      });
+
+      context.read<PostIndexModel>().setLayout(layout);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
 
     Future.microtask(() {
-      context.read<PostIndexModel>().getPosts();
+      context.read<PostIndexModel>().getPosts(
+            sort: widget.sort ?? 'latest',
+          );
+    
+    // 恢复布局 
+    restoreLayout();
     });
   }
 
@@ -29,6 +55,28 @@ class _PostListState extends State<PostList> {
       ),
     );
 
+    final stackList = ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return PostListItem(item: posts[index]);
+      },
+    );
+
+    final gridList = GridView.builder(
+      itemCount: posts.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+      ),
+      itemBuilder: (context, index) {
+        return PostListItem(
+          item: posts[index],
+          layout: PostListLayout.grid,
+        );
+      },
+    );
+
     final list = ListView.builder(
       itemCount: posts.length,
       itemBuilder: (context, index) {
@@ -36,6 +84,12 @@ class _PostListState extends State<PostList> {
       },
     );
 
-    return posts.length == 0 ? noContent : list;
+    Widget postList = stackList;
+
+    if (model.layout == PostListLayout.grid) {
+      postList = gridList;
+    }
+
+    return posts.length == 0 ? noContent : postList;
   }
 }
